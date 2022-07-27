cdfa6d5d37ab039638f69c2952f2f5419a9e3080
Павел <fotocrysis36@gmail.com>
2022-07-27 09:47:19 +0300

    Lesson 36

--- todolist/settings.py
+++ todolist/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'social_django',
     'goals',
+    'bot',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -159,3 +161,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+BOT_TOKEN = env("BOT_TOKEN")
